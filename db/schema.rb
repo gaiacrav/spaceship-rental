ea3cb44b71d064654d6427dbf35b487f2c3ef55c
Gaia Cravesana <gcravesana@gmail.com>
2021-02-22 17:17:28 +0000

    Created the Users and Spaceships models and run the migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_163617) do
+ActiveRecord::Schema.define(version: 2021_02_22_171316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "spaceships", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "destination"
+    t.string "name"
+    t.integer "price"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_spaceships_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +34,12 @@ ActiveRecord::Schema.define(version: 2021_02_22_163617) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "spaceships", "users"
 end
