ed8bdfb1782f21d9187f9f98dbd7f1d9b6cdf202
Eliseu Mateus <eliseumateus@gmail.com>
2021-02-25 12:43:40 +0000

    Made the serch and changed the images

--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -6,4 +6,12 @@ class Spaceship < ApplicationRecord
   validates :name,  presence: true, uniqueness: true
   validates :destination, inclusion: { in: %w(mars saturn mercury venus uranus neptune moon), message: "Please type a valid Planet!" }
   validates :price, numericality: { only_integer: true }
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :name, :destination ],
+    using: {
+      tsearch: { prefix: true }
+    }
+
 end
