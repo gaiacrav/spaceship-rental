c3710730256c739d0eee867e196b12254454706d
Eliseu Mateus <eliseumateus@gmail.com>
2021-02-26 12:20:07 +0000

    hotdeals, one booking seed, if else stantements in index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
+    # @spaceships = Spaceship.where(hotdeal: true) # need to create a new column in Spaceships
+    @spaceships = Spaceship.where('price > 89000')
   end
 
   def my_bookings
